566c5b4334789cd32cee7f041f7151a5c876867e
Andy Lee <a.lee.intl@gmail.com>
2023-12-12 22:51:44 +0100

    string/normalizeStr

---
+++ src/common/string/normalizeStr.ts
@@ -0,0 +1,13 @@
+export const normalizeStr = (
+  str: string,
+  unifyCase?: "uppercase" | "lowercase"
+) => {
+  const res = str.normalize("NFKD").replace(/[\u0300-\u036f]/g, "")
+  if (unifyCase === "uppercase") {
+    return res.toUpperCase()
+  }
+  if (unifyCase === "lowercase") {
+    return res.toLowerCase()
+  }
+  return res
+}
